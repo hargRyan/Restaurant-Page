235b5f52954c1a041d639c53b48e2543aa73896a
Ryan Hargreaves <hargryan@protonmail.com>
2021-07-16 12:59:35 -0700

    changes from night before to start fresh

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("Hello world!");
+import './style.css';
+
+function test() {
+    const element = document.createElement('div');
+
+    element.textContent = "Hello world!"
+    element.classList.add('hello');
+
+    return element;
+}
+
+document.body.appendChild(test());
